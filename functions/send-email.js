0a0a367f42353f99381699de6a63955c80f995fb
Asad1021 <104274350+Asad1021@users.noreply.github.com>
2024-08-13 02:17:02 +0530

    error

--- functions/send-email.js
+++ functions/send-email.js
@@ -1,40 +1,40 @@
-const sgMail = require('@sendgrid/mail');
+// const sgMail = require('@sendgrid/mail');
 
-exports.handler = async (event, context) => {
-  if (event.httpMethod !== 'POST') {
-    return { statusCode: 405, body: 'Method Not Allowed' };
-  }
+// exports.handler = async (event, context) => {
+//   if (event.httpMethod !== 'POST') {
+//     return { statusCode: 405, body: 'Method Not Allowed' };
+//   }
 
-  const { name, email, phone, message } = JSON.parse(event.body);
+//   const { name, email, phone, message } = JSON.parse(event.body);
 
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+//   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const msg = {
-    to: 'destination-email@example.com', // Replace with your email
-    from: process.env.SENDGRID_VERIFIED_SENDER, // Must be verified in SendGrid
-    subject: 'New Contact Form Submission',
-    text: `
-      Name: ${name}
-      Email: ${email}
-      Phone: ${phone}
-      Message: ${message}
-    `,
-  };
+//   const msg = {
+//     to: 'destination-email@example.com', // Replace with your email
+//     from: process.env.SENDGRID_VERIFIED_SENDER, // Must be verified in SendGrid
+//     subject: 'New Contact Form Submission',
+//     text: `
+//       Name: ${name}
+//       Email: ${email}
+//       Phone: ${phone}
+//       Message: ${message}
+//     `,
+//   };
 
-  try {
-    await sgMail.send(msg);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'Email sent successfully' })
-    };
-  } catch (error) {
-    console.error('Error sending email:', error);
-    if (error.response) {
-      console.error(error.response.body)
-    }
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Error sending email' })
-    };
-  }
-};
+//   try {
+//     await sgMail.send(msg);
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({ message: 'Email sent successfully' })
+//     };
+//   } catch (error) {
+//     console.error('Error sending email:', error);
+//     if (error.response) {
+//       console.error(error.response.body)
+//     }
+//     return {
+//       statusCode: 500,
+//       body: JSON.stringify({ error: 'Error sending email' })
+//     };
+//   }
+// };
